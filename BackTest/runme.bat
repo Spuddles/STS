f5a3a485cd0872488fce6839069cada00606a83c
Spud <Spuddles666@gmail.com>
2018-02-14 11:37:28 +0000

    Made updating script for new coin pairs easier

--- BackTest/runme.bat
+++ BackTest/runme.bat
@@ -1,20 +1,8 @@
-x64\Debug\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-BNB.json
-type Crumble-BNB.log | find "POSITION, BTC" > Position-BNB.csv
+call runsingle ETH
+call runsingle LTC
+call runsingle BNB
+call runsingle NEO
 
-x64\Debug\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-ETH.json
-type Crumble-ETH.log | find "POSITION, BTC" > Position-ETH.csv
-
-x64\Debug\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-IOTA.json
-type Crumble-IOTA.log | find "POSITION, BTC" > Position-IOTA.csv
-
-x64\Debug\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-LTC.json
-type Crumble-LTC.log | find "POSITION, BTC" > Position-LTC.csv
-
-x64\Debug\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-NEO.json
-type Crumble-NEO.log | find "POSITION, BTC" > Position-NEO.csv
-
-x64\Debug\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-CND.json
-type Crumble-CND.log | find "POSITION, BTC" > Position-CND.csv
 gnuplot PnL.plot > PnL.png
 
 start PnL.png
