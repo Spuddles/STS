aadc65c434418444d0a6ad13a2a099868b125ca6
Spud <Spuddles666@gmail.com>
2018-03-14 23:03:07 +0000

    Fixed broken bitmask operation

--- BackTest/runrangeall.bat
+++ BackTest/runrangeall.bat
@@ -1,4 +1,4 @@
-rem runrange.bat TRX
+call runrange.bat TRX
 call runrange.bat ETH
 call runrange.bat NANO
 call runrange.bat ZIL
@@ -6,7 +6,7 @@ call runrange.bat NCASH
 call runrange.bat IOST
 call runrange.bat XRP
 call runrange.bat BNB
-rem runrange.bat LTC
+call runrange.bat LTC
 call runrange.bat BCC
 call runrange.bat NEO
 call runrange.bat WTC
