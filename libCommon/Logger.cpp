ebc21c2016bb40be05470b424b65abfcdf2bca7c
Spud <Spuddles666@gmail.com>
2018-03-24 11:57:10 +0000

    Allow setting of log level from the config file

--- libCommon/Logger.cpp
+++ libCommon/Logger.cpp
@@ -4,7 +4,7 @@
 
 using namespace STS;
 
-Logger::LogLevel Logger::m_logLevel = LogLevel::NORM;
+Logger::LogLevel Logger::m_logLevel = LogLevel::NORMAL;
 std::ofstream	 Logger::m_defaultLogFile;
 
 Logger::Logger()
@@ -54,6 +54,30 @@ void Logger::setLogLevel(int level)
 	m_logLevel = static_cast<LogLevel>(level);
 }
 
+void Logger::setLogLevel(const std::string &level)
+{
+	m_logLevel = NONE;
+
+	size_t start = 0;
+	size_t pos = 0;
+	std::string temp;
+	while (pos != std::string::npos)
+	{
+		pos = level.find("|", start);
+		if (pos == std::string::npos)
+		{
+			temp = level.substr(start);
+			m_logLevel = static_cast<LogLevel>(m_logLevel | getLogLevelFromStr(temp));
+		}
+		else
+		{
+			temp = level.substr(start, (pos-start));
+			m_logLevel = static_cast<LogLevel>(m_logLevel | getLogLevelFromStr(temp));
+			start = pos + 1;
+		}
+	}
+}
+
 void Logger::clearLogMask()
 {
 	m_logLevel = static_cast<LogLevel>(0);
@@ -69,12 +93,12 @@ void Logger::fillLogMask()
 	m_logLevel = static_cast<LogLevel>(-1);
 }
 
-std::string Logger::getLogLevelStr(LogLevel level)
+std::string Logger::getLogLevelStr(Logger::LogLevel level)
 {
 	switch (level)
 	{
 	case	DEBUG:		return "DEBUG";
-	case	NORM:		return "NORMAL";
+	case	NORMAL:		return "NORMAL";
 	case	WARN:		return "WARN";
 	case	ERROR:		return "ERROR";
 	case	CRITICAL:	return "CRITCIAL";
@@ -86,4 +110,20 @@ std::string Logger::getLogLevelStr(LogLevel level)
 	case	PNL:		return "PNL";
 	default: return "UNKNOWN";
 	}
+}
+
+Logger::LogLevel Logger::getLogLevelFromStr(const std::string &logLevel)
+{
+	if (logLevel.compare("DEBUG") == 0)	return DEBUG;
+	else if (logLevel.compare("NORMAL") == 0)	return NORMAL;
+	else if (logLevel.compare("WARN") == 0)	return WARN;
+	else if (logLevel.compare("ERROR") == 0)	return ERROR;
+	else if (logLevel.compare("CRITICAL") == 0)	return CRITICAL;
+	else if (logLevel.compare("TRADE") == 0)	return TRADE;
+	else if (logLevel.compare("POSITION") == 0)	return POSITION;
+	else if (logLevel.compare("SIGNAL") == 0)	return SIGNAL;
+	else if (logLevel.compare("ALGO") == 0)	return ALGO;
+	else if (logLevel.compare("EXCHANGE") == 0)	return EXCHANGE;
+	else if (logLevel.compare("PNL") == 0)	return PNL;
+	return NONE;
 }
--- libCommon/Logger.h
+++ libCommon/Logger.h
@@ -13,8 +13,9 @@ namespace STS
 
 		enum LogLevel : unsigned int
 		{
+			NONE		= 0,
 			DEBUG		= 1,
-			NORM		= 2,
+			NORMAL		= 2,
 			WARN		= 4,
 			ERROR		= 8,
 			CRITICAL	= 16,
@@ -33,14 +34,16 @@ namespace STS
 		static void setDefaultLog(const std::string &filename);
 		static void	log(LogLevel level, const std::string &msg);
 		static void	setLogLevel(int level);
+		static void	setLogLevel(const std::string &level);
 		static void clearLogMask();
 		static void addLogMask(LogLevel level);
 		static void fillLogMask();
 
 	private:
-		static std::string	getLogLevelStr(LogLevel level);
+		static std::string		getLogLevelStr(Logger::LogLevel level);
+		static Logger::LogLevel	getLogLevelFromStr(const std::string &logLevel);
 
-		static LogLevel		m_logLevel;
+		static LogLevel			m_logLevel;
 
 		// Store the file handles for the different log types
 		static std::ofstream	m_defaultLogFile;
