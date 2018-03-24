ce9faf334335763e18e16d564c5222077ff9457d
Spud <Spuddles666@gmail.com>
2018-03-24 11:57:55 +0000

    Naming changes for config files

--- BackTest/runsingle.bat
+++ BackTest/runsingle.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 REM %1 is the coin code for this run
-x64\Debug\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-%1.json
-type Crumble-%1.log | find "POSITION, BTC" > Position-%1.csv
-type Crumble-%1.log | find "ALGO" > Algo-%1.log
-type Crumble-%1.log | find "SIGNAL" > Signal-%1.log
-type Crumble-%1.log | find "Sells:"
-type Crumble-%1.log | find "Total BTC value"
+x64\Release\BackTest.exe -DB ..\Trading.sqlite -CONFIG ..\strategyConfigs\Crumble-%1-Live.json
+type CrumbleStrat-%1BTC.log | find "POSITION, BTC" > Position-%1.csv
+type CrumbleStrat-%1BTC.log | find "ALGO" > Algo-%1.log
+rem type CrumbleStrat-%1BTC.log | find "SIGNAL" > Signal-%1.log
+rem type CrumbleStrat-%1BTC.log | find "Sells:"
+rem type CrumbleStrat-%1BTC.log | find "Total BTC value"
 gnuplot %1-PnL.plot > %1-PnL.png
